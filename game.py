f8f3c524b78326a2639abd77adb04ffe96c32b9e
John McCabe Nolan <john.mccabe.nolan@outlook.com>
2025-07-20 10:49:23 +0100

    first working game

--- game.py
+++ game.py
@@ -1,19 +1,31 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import random
+import os
 
-players = ["lahm", "messi", "ronaldo"]
+players = [f for f in os.listdir("players") if f.endswith(".png")]
+random_index = random.randint(0, 9)
 
-for i in range(len(players)):
-    player = players[i]
+while len(players) >= 1:
+    i = len(players)
 
-    img = mpimg.imread(f"players/{player}_teammates.jpg")
+    random_index = random.randint(0, i - 1)
+    player = players[random_index]
+    player = os.path.splitext(player)[0]
+
+    print("Guess the player by his teammates!")
+
+    img = mpimg.imread(f"teammates/{player}.png")
     plt.imshow(img)
-    plt.axis('off')  # Hide axis
-    plt.title(f"{player}")
+    plt.axis('off')
     plt.show()
-# Load and display an image
 
-img = mpimg.imread(f"players/lahm_teammates.png")
-plt.imshow(img)
-plt.axis('off')  # Hide axis
-plt.show()
+    user_input = input("Who do you think it is? ")
+
+    if user_input == player:
+        print(f"{player} is correct! Nice job, can you guess the next one correctly? ")
+    else:
+        print("Not quite! Try again next time :/")
+
+    # Remove the selected player
+    del players[random_index]
